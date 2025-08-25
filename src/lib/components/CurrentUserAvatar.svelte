212e8566607286ebd9fcd2bfc3d2961920ebfc11
Josué Licona <j.liconam@outlook.com>
2025-08-25 02:31:52 +0000

    Create CurrentUserAvatar component

---
+++ src/lib/components/CurrentUserAvatar.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { appState } from '$lib/state.svelte';
+
+	let { children = null } = $props();
+
+	/**
+	 * Returns initials of a name
+	 * @param {string} name
+	 * @returns {string}
+	 */
+	function getAvatarInitials(name) {
+		const parts = name.split(' ');
+		let firstLetter = ':';
+		let secondLetter = '(';
+		if (parts.length == 1) {
+			firstLetter = name[0];
+			secondLetter = name[1];
+		} else if (parts.length > 1) {
+			firstLetter = parts[0][0];
+			secondLetter = parts[parts.length - 1][0];
+		}
+		return firstLetter?.toUpperCase() + secondLetter?.toUpperCase();
+	}
+
+	let initials = $state(':(');
+	if (children == null && appState.currentPerson != null) {
+		initials = getAvatarInitials(appState.currentPerson.name);
+	}
+</script>
+
+<div class="avatar avatar-placeholder">
+	<div class="w-12 rounded-full bg-primary text-primary-content hover:bg-primary/80">
+		{#if children}
+			{@render children()}
+		{:else}
+			<span>{initials}</span>
+		{/if}
+	</div>
+</div>
