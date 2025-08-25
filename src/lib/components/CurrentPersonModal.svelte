6f6a2eb5a1aa34db313ffc1efb3895b0c242b70b
Josué Licona <j.liconam@outlook.com>
2025-08-25 02:32:15 +0000

    Create CurrentPersonModal component

---
+++ src/lib/components/CurrentPersonModal.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { appState } from '$lib/state.svelte';
+	import CurrentUserAvatar from './CurrentUserAvatar.svelte';
+	import Modal from './Modal.svelte';
+
+	let { dialog = $bindable() } = $props();
+</script>
+
+<Modal bind:dialog>
+	<div class="inline-flex items-center gap-4">
+		<CurrentUserAvatar />
+		<h1 class="text-xl">{appState.currentPerson?.name}</h1>
+	</div>
+
+	<!--Botón de continuar-->
+	<div class="modal-action flex w-full">
+		<button class="btn flex-1 btn-soft btn-secondary"> Salir </button>
+		<button class="btn flex-1 btn-soft btn-neutral"> Cambiar </button>
+	</div>
+</Modal>
