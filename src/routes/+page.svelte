6afab1bb898e3e53c28329f4d3602ddbe16ef95d
Josué Licona <j.liconam@outlook.com>
2025-08-26 02:47:04 +0000

    Greet current person

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { appState } from '$lib/state.svelte';
+</script>
+
+{#if appState.currentPerson !== null}
+	<h1 class="text-3xl text-base-content/60">Hola, {appState.currentPerson.name}</h1>
+{/if}
